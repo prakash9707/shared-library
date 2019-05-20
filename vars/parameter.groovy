0af9ac9cfb5690f6ef2b1e68e73d51a7cdc9f6fe
prakash9707 <41231971+prakash9707@users.noreply.github.com>
2019-05-20 10:21:05 +0530

    Update parameter.groovy

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -1,4 +1,35 @@
 
-def call(){
-    return [gitRepo : 'https://github.com/prakash9707/springboot.git/', dockerimg : 'img', server: 'localhost:8000/api']
+def call(Map pipelineParams){
+node{
+def mvnHome
+stage('git checkout process'){
+  echo 'started checkout'
+  echo pipelineParams.gitRepo
+  echo pipelineParams.dockerimg
+  echo pipelineParams.server
+  git pipelineParams.gitRepo
+  echo 'completed sucessfully'
+}
+  
+  stage('SonarQube analysis') {
+    withSonarQubeEnv('sonarqube') {
+      mvnHome = '/opt/apache-maven/bin'
+      sh "${mvnHome}/mvn sonar:sonar"
+      
+    }
+  }
+  
+  stage("Quality Gate"){
+  timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
+    def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+    if (qg.status != 'OK') {
+      error "Pipeline aborted due to quality gate failure: ${qg.status}"
+    }
+  }
+}
+
+stage('Build') {
+    echo 'hai over'
+  }
+}
 }
