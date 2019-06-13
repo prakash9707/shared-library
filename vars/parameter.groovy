9c597ff36f635c3770353b516c71e8d1085b5aa8
prakash9707 <41231971+prakash9707@users.noreply.github.com>
2019-06-13 10:21:56 +0530

    Update parameter.groovy

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -3,6 +3,11 @@ def call(Map pipelineParams){
 node{
   stage('hello'){
     def PUBLIC_FOLDER_PATH = "hello"
+    Date date = new Date()
+    String datePart = date.format("dd/MM/yyyy")
+    String timePart = date.format("HH:mm:ss")
+    echo datePart
+    echo timePart
    def VERSION_CONTENT = 'Build Id - ' + '\n' + 'Build Triggered By - ' + '\n' + 'Application URL - ' + '\n' 
    writeFile file: "${PUBLIC_FOLDER_PATH}/Version.txt", text: VERSION_CONTENT 
   }
