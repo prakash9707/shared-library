7b98496a04dc60a239e75a75d31ab8ec885f6fa0
prakash9707 <41231971+prakash9707@users.noreply.github.com>
2019-06-13 10:35:16 +0530

    Update parameter.groovy

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -6,6 +6,8 @@ node{
     Date date = new Date()
     String datePart = date.format("dd/MM/yyyy")
     String timePart = date.format("HH:mm:ss")
+    int a = 10;
+    echo a
     echo datePart
     echo timePart
    def VERSION_CONTENT = 'Build Id - ' + '\n' + 'Build Triggered By - ' + '\n' + 'Application URL - ' + '\n' + 'Date of build - ' + datePart + '\n' + 'time of build - ' + timePart + '\n' 
