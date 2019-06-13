636227976cd125079185b2ac0b9a8e6979ffa637
prakash9707 <41231971+prakash9707@users.noreply.github.com>
2019-06-13 09:57:10 +0530

    Update parameter.groovy

--- vars/parameter.groovy
+++ vars/parameter.groovy
@@ -1,35 +1,10 @@
 
 def call(Map pipelineParams){
 node{
-def mvnHome
-stage('git checkout process'){
-  echo 'started checkout'
-  echo pipelineParams.gitRepo
-  echo pipelineParams.dockerimg
-  echo pipelineParams.server
-  git pipelineParams.gitRepo
-  echo 'completed sucessfully'
-}
-  
-  stage('SonarQube analysis') {
-    withSonarQubeEnv('sonarqube') {
-      mvnHome = '/opt/apache-maven/bin'
-      sh "${mvnHome}/mvn sonar:sonar"
-      
-    }
-  }
-  
-  stage("Quality Gate"){
-  timeout(time: 1, unit: 'HOURS') { // Just in case something goes wrong, pipeline will be killed after a timeout
-    def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
-    if (qg.status != 'OK') {
-      error "Pipeline aborted due to quality gate failure: ${qg.status}"
-    }
-  }
-}
-
-stage('Build') {
-    echo 'hai over'
+  stage('hello'){
+    def PUBLIC_FOLDER_PATH = "hello"
+   def VERSION_CONTENT = 'Build Id - ' + '\n' + 'Build Triggered By - ' + '\n' + 'Application URL - ' + '\n' 
+   writeFile file: "${PUBLIC_FOLDER_PATH}/Version.txt", text: VERSION_CONTENT 
   }
 }
 }
